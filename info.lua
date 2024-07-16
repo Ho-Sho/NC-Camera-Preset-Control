a643e04d6b83a675881bfea44c82e5ff514666c1
Ho-Sho <98727088+Ho-Sho@users.noreply.github.com>
2024-07-17 02:02:34 +0900

    Add the feature for Presets Equal Position LED.

--- info.lua
+++ info.lua
@@ -1,9 +1,9 @@
 PluginInfo = {
-  Name = "Tools~NC Camera Preset Control",
-  Version = "1.0.0",
-  BuildVersion = "1.0.0.0",
+  Name = "Tools~NC Camera Preset Control v1.0.1",
+  Version = "1.0.1",
+  BuildVersion = "1.0.1.0",
   Id = "6a96f761-aff0-483b-8a06-290e61e4c272",--<guid>
   Author = "Hori Shogo",
-  Description = "NC Camera Preset Control \nv1.0.0",
+  Description = "NC Camera Preset Control \nv1.0.1",
   ShowDebug = true,
 }
